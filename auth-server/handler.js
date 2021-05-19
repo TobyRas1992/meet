336e7cdec954f7a293aed3e14a614677253804f4
TobyRas1992 <tobiasrasmussen@outlook.com>
2021-05-19 13:04:30 +0200

    slight changes - removed a few comments

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -1,5 +1,6 @@
 const { google } = require("googleapis");
-const OAuth2 = google.auth.OAuth2;
+// const { resultingClientExists } = require("workbox-core/_private"); //QUESTION: What does this do?
+const OAuth2 = google.auth.OAuth2; // is a authentication and authorization protocol
 const calendar = google.calendar("v3");
 const SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"];
 
@@ -13,17 +14,18 @@ const credentials = {
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-  redirect_uris: ["https://tobyras1992.github.io/meet/"],
+  redirect_uris: ["https://tobyras1992.github.io/meet/"], // redirects user after successful registration
   javascript_origins: ["https://tobyras1992.github.io", "http://localhost:3000"],
 };
 
-const { client_secret, client_id, redirect_uris, calendar_id } = credentials;
+const { client_secret, client_id, redirect_uris, calendar_id } = credentials; // 
 const oAuth2Client = new google.auth.OAuth2(
   client_id,
   client_secret,
-  redirect_uris[0]
+  redirect_uris[0] // question: does this specify the only available option?
 );
 
+// STEP 1
 module.exports.getAuthURL = async () => {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
@@ -33,7 +35,7 @@ module.exports.getAuthURL = async () => {
   return {
     statusCode: 200,
     headers: {
-      "Access-Control-Allow-Origin": "*",
+      'Access-Control-Allow-Origin': '*',
     },
     body: JSON.stringify({
       authUrl: authUrl,
@@ -41,13 +43,13 @@ module.exports.getAuthURL = async () => {
   };
 };
 
+// STEP 2
 module.exports.getAccessToken = async (event) => {
   const oAuth2Client = new google.auth.OAuth2(
     client_id,
     client_secret,
     redirect_uris[0]
   );
-
   const code = decodeURIComponent(`${event.pathParameters.code}`);
 
   return new Promise((resolve, reject) => {
@@ -74,14 +76,49 @@ module.exports.getAccessToken = async (event) => {
   });
 };
 
+//STEP 3
 module.exports.getCalendarEvents = async (event) => {
-return new Promise((resolve,reject) =>{
+  const oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
 
-const oAuth2Client = new google.auth.Oauth2(client_id, client_secret, redirect_uris[0]);
+const access_token = decodeURIComponent(`${event.pathParameters.access_token}`); //gets token from URI
 
-const access_token = decodeURIComponent(`${event.pathParameters.access_token}`);
+oAuth2Client.setCredentials({access_token}); //sets access token as credentials
 
-oAuth2Client.setCredentials({access_token});
-
+return new Promise ((resolve, reject) =>{
+  calendar.events.list(
+    {
+      calendarId: calendar_id,
+      auth: oAuth2Client,
+      timeMin: new Date().toISOString(),
+      singleEvents: true,
+      orderBy: "startTime",
+    },
+    (error, response) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve (response);
+      }
+    }
+  );
+}).then((results) => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+    },
+    body: JSON.stringify({events: results.data.items}),
+  };
+}
+).catch((err) =>{
+  console.log(err);
+  return{
+    statusCode: 500,
+    body: JSON.stringify(err),
+  }; 
 });
 };
