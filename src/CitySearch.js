3ad62119ea63bb53cd6b9579bff745fb66105714
TobyRas1992 <tobiasrasmussen@outlook.com>
2021-06-09 22:17:05 +0200

    finished lesson, but missing two tests still

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,7 +3,8 @@ import React, {Component} from 'react';
 class CitySearch extends Component {
   state = {
     query: '',
-    suggestions: []
+    suggestions: [],
+    showSuggestions: undefined
   }
 
   handleInputChanged = (event) => { 
@@ -15,7 +16,8 @@ class CitySearch extends Component {
 
   handleItemClicked = (suggestion) => {
     this.setState({
-      query: suggestion
+      query: suggestion,
+      showSuggestions: false
     });
     this.props.updateEvents(suggestion); // sets events state with events with same location as selected city. 
   }
@@ -23,11 +25,15 @@ class CitySearch extends Component {
   render() {
     return (
       <div className="CitySearch">
-        <input type="text"className="city" value={this.state.query} onChange={this.handleInputChanged} /> {/* all textObjects have a value property */}
-        <ul className="suggestions">
+        <input type="text"
+        className="city"
+        value={this.state.query}
+        onChange={this.handleInputChanged}
+        onFocus = {() => { this.setState({ showSuggestions: true})}}/> 
+        <ul className="suggestions" style={this.state.showSuggestions ? {}: {display: 'none'}}>
           {this.state.suggestions.map((suggestion) => (
             <li key={suggestion}
-            onClick={() => this.handleItemClicked(suggestion)}>{suggestion}</li>))} 
+            onClick={() => this.handleItemClicked(suggestion)}>{suggestion}</li>))} {/* if showSuggestions is true, list will be visible */}
             {/* onClick holds a reference to handleItemCLicked(), we must wrap it in an outer function to pass in a named argument */}
           <li onClick={() => this.handleItemClicked('all')}>
             <b>See all cities</b>
--- src/api.js
+++ src/api.js
@@ -1,4 +1,6 @@
 import { mockData } from './mock-data';
+import axios from 'axios';
+import NProgress from 'nprogress';
 
 export const extractLocations = (events) => { //extracts event location from an array of events and remove duplicates.
   var extractLocations = events.map((event) => event.location); //creates new array only with locations.
@@ -6,6 +8,75 @@ export const extractLocations = (events) => { //extracts event location from an 
   return locations;
 };
 
-export const getEvents = async () => {
-  return mockData;
+const checkToken = async (accessToken) => {
+  const result = await fetch(`https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`)
+  .then((res) => res.json())
+  .catch((error) => error.json());
+  return result; 
 };
+
+const getToken = async (code) => { //fetches new token for user. 
+  const encodeCode = encodeURIComponent(code); // encodes your code.
+  const {access_token} = await fetch('https://qg6namd6jb.execute-api.eu-central-1.amazonaws.com/dev/api/token' + '/' + encodeCode)
+  .then((res) => {
+    return res.json();
+  })
+  .catch((error) => error);
+
+  access_token && localStorage.setItem('access_token', access_token);
+
+  return access_token;
+};
+
+const removeQuery = () => { // removes code from URL once I'm finished with it. Function checks whether there's a path, then builds the URL with the current path (or builds the URL without a path using window.history.pushState()).
+  if (window.history.pushState && window.location.pathname) {
+    var newurl = window.location.protocol + '//' + window.location.host + window.location.pathname; 
+    window.history.pushState('','', newurl);
+  } else {
+    newurl= window.location.protocol + '//' + window.location.host;
+    window.history.pushState('', '', newurl);
+  }
+}
+
+export const getEvents = async () => {
+  NProgress.start(); // displays progress bar at the top of page. 
+
+  if(window.location.href.startsWith('http://localhost')) { //ensures we return mockData (and not real api) if using localhost. 
+    return mockData;
+  }
+
+  const token = await getAccessToken(); // check for access token. 
+
+  if (token) { // if token exists, make GET request to Google Calendar API. 
+    removeQuery();
+    const url = 'https://qg6namd6jb.execute-api.eu-central-1.amazonaws.com/dev/api/get-events' + '/' + token;
+    const result = await axios.get(url);
+    if(result.data) {
+      var locations = extractLocations(result.data.events);
+      localStorage.setItem('lastEvents', JSON.stringify(result.data));
+      localStorage.setItem('locations', JSON.stringify(locations));
+    }
+    NProgress.done();
+    return result.data.events;
+  }  
+};
+
+export const getAccessToken = async () => {
+  const accessToken = localStorage.getItem('access_token'); //checks localstorage first for pre existing access token. 
+
+  //step 2: if no accesstoken is found in localstorage. 
+  const tokenCheck = accessToken && (await checkToken(accessToken)); // John: does this check the validity of the localstorage access token?
+
+  if (!accessToken || tokenCheck.error) { // checks if access token is found, then if authorization code is found. 
+    await localStorage.removeItem('access_token');
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = await searchParams.get('code'); //gets code from search params. 
+    if (!code) { // if no authorization code is found. 
+      const results = await axios.get('https://qg6namd6jb.execute-api.eu-central-1.amazonaws.com/dev/api/get-auth-url'); // update with correct url - John: do I need to remove get aut url? 
+      const { authUrl } = results.data;
+      return (window.location.href = authUrl); // redirects user to Google Authorization screen.
+    }
+    return code && getToken(code);
+  }
+  return accessToken;
+}
