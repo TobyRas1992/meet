a1f7ed360fd27a86341180583b16e8656b14dcfa
TobyRas1992 <tobiasrasmussen@outlook.com>
2021-06-23 14:47:36 +0200

    added samle chart code

--- src/App.js
+++ src/App.js
@@ -68,6 +68,17 @@ class App extends Component {
         <p>Choose your nearest city</p>
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} numberOfEvents={this.state.numberOfEvents} />
         <NumberOfEvents updateNumberOfEvents={(e) => this.updateNumberOfEvents(e)} />
+        <h4>Events in each city</h4>
+        <ScatterChart
+          width={400}
+          height={400}
+          margin={{ top: 20, right: 20, bottom: 20, left: 20 }}>
+          <CartesianGrid />
+          <XAsis type="number" dataKey="x" name="stature" unit="cm" />
+          <YAxis type="number" dataKey="y" name="weight" unit="kg" />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter name="A school" data={data} fill="#8884d8" />
+        </ScatterChart>
         <EventList events={this.state.events} />
       </div>
     );
